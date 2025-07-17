3fe55f2241592f9cf382135c115b1f13d43af054
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-17 14:43:50 -0300

    8 - refactor: Replace JsonArray with Jackson in AbrigoService.

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
@@ -10,5 +11,13 @@ public class Abrigo {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
     }
 }
