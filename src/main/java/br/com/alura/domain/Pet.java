ec6bae3210f13a56124eb9ec0ee8d2eeda324bb3
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-17 13:53:08 -0300

    7 - Using Pet in PetService.

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,4 +1,21 @@
 package br.com.alura.domain;
 
 public class Pet {
+
+    private String nome;
+    private String tipo;
+    private String raca;
+    private String cor;
+    private Float peso;
+    private int idade;
+
+    public Pet(String nome, String tipo, String raca, int idade, String cor, Float peso) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
 }
